1694df8e3730d879627afc29fc81054104d1e729
Kage-hime <changweesiangwilson@gmail.com>
2022-02-14 23:16:50 +0800

    File Reading Test from Java project

---
+++ File Reading Test/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) throws FileNotFoundException {
+
+        ArrayList<String> playerlist = new ArrayList<String>();
+        ArrayList<Float> playerScore = new ArrayList<Float>();
+
+        File file = new File("src\\namelist.txt");
+        Scanner filescan = new Scanner(file);
+
+        while(filescan.hasNextLine())
+        {
+            playerlist.add(filescan.nextLine());
+        }
+
+        file = new File("src\\scorelist.txt");
+        filescan = new Scanner(file);
+
+        while(filescan.hasNextLine())
+        {
+            playerScore.add(filescan.nextFloat());
+        }
+
+
+        for(int i = 0 ; i < playerlist.size() ; i++)
+            System.out.format(playerlist.get(i) + " %.0f\n", playerScore.get(i) );
+
+        filescan.close();
+
+    }
+}
